ec862d3291121b49468333eb1854899a6fb00c8b
Alicja Krzesniak <ala.krzesniak@gmail.com>
2017-04-10 19:55:26 +0200

    Rysowanie SFOAE krotkich w 3D dP(f)(t), malo czytelny wykres

--- analiza_krotkie_dzien.m
+++ analiza_krotkie_dzien.m
@@ -1,30 +1,40 @@
-%comparison of averages and variances for different days 
+
+%3d plot for different times
 wczytanie;
+%a-1 = number of short trials
 n = 4; %data.cfg.numf
-d = 3; %number of days
-L = zeros(d,2*n);
-R = zeros(d,2*n); %n values and n variances for each day
+%d = 3; %number of days
+L = zeros(a-1,n);
+R = zeros(a-1,n); %n values and n variances for each day
+times_l = zeros(a-1,1);
+times_r = zeros(a-1,1);
 l = 0;
 r = 0;
 for i=1:a-1
     data = short{i,3};
     day = short{i,2};
+    hour =short{i,4}(1:4);
+    y = real(data.sfe.dP);
     if short{i,1}=='L'
-        y = abs(data.sfe.dP).^2;
-        plot(data.sfe.fp,y,'s-.'); title('Left ear')
-        ylabel('Power, dP^2')
-        L = L + y;
+        L(l+1,:) = y;
+        times_l(l+1) = str2double(day);
         l = l + 1;
     else
-        subplot(2,1,2); xlim([800 4200]);
-        y = abs(data.sfe.dP).^2;
-        plot(data.sfe.fp,y,'o-.'); title('Right ear')
-        ylabel('Power, dP^2')
-        xlabel('Frequency [Hz]')
-        R = R + y;
+        R(r+1,:) = y;
+        %times_r(r+1) = str2double(strcat(day, hour));
         r = r + 1;
     end
 end
-subplot(2,1,1); xlim([800 4200]);
-subplot(2,1,1);plot(data.sfe.fp,L./l,'r', 'LineWidth', 1.5)
-subplot(2,1,2);plot(data.sfe.fp,R./r,'r', 'LineWidth', 1.5); 
+t_l = datetime(times_l,'ConvertFrom','yyyymmdd');
+surf([1:l],data.sfe.fp,L(1:l,:)'); title('Left ear') %mesh
+xlabel('trial')
+ylabel('Frequency [Hz]')
+zlabel('dP')
+colorbar()
+% plot(data.sfe.fp,y,'o-.'); title('Right ear')
+% ylabel('Power, dP^2')
+% xlabel('Frequency [Hz]')
+% ylabel('dP')
+% subplot(2,1,1); xlim([800 4200]);
+% subplot(2,1,1);plot(data.sfe.fp,L./l,'r', 'LineWidth', 1.5)
+% subplot(2,1,2);plot(data.sfe.fp,R./r,'r', 'LineWidth', 1.5); 
