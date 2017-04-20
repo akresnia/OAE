587323b669d664f0376422c2a64f4897d46eacad
Alicja Krzesniak <ala.krzesniak@gmail.com>
2017-04-20 11:55:08 +0200

    longer trials analysis for both ears
    nicer version managing both ears with a loop. Plots of each trial and
    boxplots

--- analiza_dlugie.m
+++ analiza_dlugie.m
@@ -2,44 +2,39 @@ wczytanie;
 %b - 1 - # of long trials
 n = 25; %length(data.sfe.fp)
 m = 5; %length(data.sfe.fclist)
-general_L = zeros(1,n);
-sL = zeros(1,m);
-small_L = zeros(1,m);
-general_R = zeros(1,n);
-sR = zeros(1,m);
-small_R = zeros(1,m);
-l = 0;
-r = 0;
+general = zeros(1,n);
+gen_mean = zeros(1,m);
+small = zeros(1,m);
+
+l.R = 0;
+l.L = 0;
+ears = ['L';'o';'R'];
 for i=1:b-1 %for each trial in dataset
     data = long{i,3};
+    y = real(data.sfe.dP);
     hold on
     for d=['L','R']
         if long{i,1}==d
-        subplot(2,2,1); xlim([800 4200]); 
-        y = real(data.sfe.dP);
-        for j=1:m
-            sL(j) = mean(y((j-1)*m+1 :(j-1)*m+m));
+            k=find(ears==d);
+            subplot(2,2,k); xlim([800 4200]);
+            for j=1:m
+                small(j) = mean(y((j-1)*m+1 :(j-1)*m+m));
+            end
+            plot(data.sfe.fclist,small,'s-.'); title([d 'ear'])
+            ylabel('dP')
+            general.(d)(l.(d)+1,:)= y;
+            gen_mean.(d)(l.(d)+1,:)= small;
+            l.(d) = l.(d) + 1;
         end
-        plot(data.sfe.fclist,sL,'s-.'); title('Left ear')
-        ylabel('dP')
-        general_L(l+1,:)= y;
-        small_L(l+1,:)= sL;
-        l = l + 1;
-    elif long{i,1}=='R'
-        subplot(2,2,3); xlim([800 4200]);
-        y = real(data.sfe.dP);
-        plot(data.sfe.fp,y,'o-.'); title('Right ear')
-        ylabel('dP')
-        xlabel('Frequency [Hz]')
-        general_R(r+1,:) = y;
-        r = r + 1;
     end
 end
+clear i j small 
+            
 subplot(2,2,1); 
-pl = plot(data.sfe.fclist,mean(small_L),'r', 'LineWidth', 1.5, 'DisplayName', 'Mean');
-%legend(pl)
-%subplot(2,2,3); 
-%pr = plot(data.sfe.fp,mean(general_R),'r', 'LineWidth', 1.5, 'DisplayName','Mean'); 
-%legend(pr)
-subplot(2,2,2); boxplot(general_L,data.sfe.fp)
-subplot(2,2,4); boxplot(general_R,data.sfe.fp)
+pl = plot(data.sfe.fclist,mean(gen_mean.L),'r', 'LineWidth', 1.5, 'DisplayName', 'Mean');
+legend(pl)
+subplot(2,2,3); 
+pr = plot(data.sfe.fclist,mean(gen_mean.R),'r', 'LineWidth', 1.5, 'DisplayName','Mean'); 
+legend(pr)
+subplot(2,2,2); boxplot(general.L,data.sfe.fp)
+subplot(2,2,4); boxplot(general.R,data.sfe.fp)
