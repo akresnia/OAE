63bb9aa15e4dc0fcb58d373fb93dbd774617db26
Alicja Krzesniak <ala.krzesniak@gmail.com>
2017-04-21 12:29:36 +0200

    shorter and unified code for long and short trial anlysis
    scripts return plots of individual trial, boxplot and scatter showing
    how dP changes in time

--- analiza_dlugie.m
+++ analiza_dlugie.m
@@ -9,6 +9,7 @@ small = zeros(1,m);
 l.R = 0;
 l.L = 0;
 ears = ['L';'o';'R'];
+figure(1)
 for i=1:b-1 %for each trial in dataset
     data = long{i,3};
     y = real(data.sfe.dP);
@@ -20,8 +21,7 @@ for i=1:b-1 %for each trial in dataset
             for j=1:m
                 small(j) = mean(y((j-1)*m+1 :(j-1)*m+m));
             end
-            plot(data.sfe.fclist,small,'s-.'); title([d 'ear'])
-            ylabel('dP')
+            plot(data.sfe.fclist,small,'s-.'); title(['Long SFOAE, "' d '"' ' ear'])
             general.(d)(l.(d)+1,:)= y;
             gen_mean.(d)(l.(d)+1,:)= small;
             l.(d) = l.(d) + 1;
@@ -33,8 +33,29 @@ clear i j small
 subplot(2,2,1); 
 pl = plot(data.sfe.fclist,mean(gen_mean.L),'r', 'LineWidth', 1.5, 'DisplayName', 'Mean');
 legend(pl)
+xlabel('Frequency [Hz]')
+ylabel('Mean dP')
 subplot(2,2,3); 
+xlabel('Frequency [Hz]')
+ylabel('Mean dP')
 pr = plot(data.sfe.fclist,mean(gen_mean.R),'r', 'LineWidth', 1.5, 'DisplayName','Mean'); 
 legend(pr)
-subplot(2,2,2); boxplot(general.L,data.sfe.fp)
-subplot(2,2,4); boxplot(general.R,data.sfe.fp)
+subplot(2,2,2); boxplot(gen_mean.L,round(data.sfe.fclist,-1))
+%xlabel('Frequency [Hz]')
+subplot(2,2,4); boxplot(gen_mean.R,round(data.sfe.fclist,-1))
+%xlabel('Frequency [Hz]')
+
+for p=1:2
+    ear = ears(2*p -1);
+    figure(p+1)
+    hold on
+    grid on
+    colorb = colorbar();
+    for i=1:m
+        scatter(1:l.(ear),ones(1,l.(ear)).*data.sfe.fclist(i),[],gen_mean.(ear)(:,i), 'filled', 'MarkerEdgeColor', 'k')
+    end
+    title(['Long SFOAE, "' ear '"' ' ear'])
+    xlabel('Number of trial')
+    ylabel('Frequency [Hz]');
+    colorb.Label.String = 'Mean dP';
+end
--- analiza_krotkie.m
+++ analiza_krotkie.m
@@ -1,28 +1,54 @@
 wczytanie;
-n = 4; %data.cfg.numf
-general_L = zeros(1,n);
-general_R = zeros(1,n);
-l = 0;
-r = 0;
-for i=1:a-1
+%a - 1 - # of short trials
+n = 4; %length(data.sfe.fp)
+general = zeros(1,n);
+
+l.R = 0;
+l.L = 0;
+ears = ['L';'o';'R'];
+figure(1)
+for i=1:a-1 %for each trial in dataset
     data = short{i,3};
+    y = real(data.sfe.dP);
     hold on
-    if short{i,1}=='L'
-        subplot(2,1,1); xlim([800 4200]); 
-        y = abs(data.sfe.dP).^2;
-        plot(data.sfe.fp,y,'s-.'); title('Left ear')
-        ylabel('Power, dP^2')
-        general_L = general_L + y;
-        l = l + 1;
-    else
-        subplot(2,1,2); xlim([800 4200]);
-        y = abs(data.sfe.dP).^2;
-        plot(data.sfe.fp,y,'o-.'); title('Right ear')
-        ylabel('Power, dP^2')
-        xlabel('Frequency [Hz]')
-        general_R = general_R + y;
-        r = r + 1;
+    for d=['L','R']
+        if short{i,1}==d
+            k=find(ears==d);
+            subplot(2,2,k); xlim([800 4200]);
+            plot(data.sfe.fp,y,'s-.'); title(['Quick SFOAE, "' d '"' ' ear'])
+            general.(d)(l.(d)+1,:)= y;
+            l.(d) = l.(d) + 1;
+        end
     end
 end
-subplot(2,1,1);plot(data.sfe.fp,general_L./l,'r', 'LineWidth', 1.5)
-subplot(2,1,2);plot(data.sfe.fp,general_R./r,'r', 'LineWidth', 1.5); 
+clear i j d
+            
+subplot(2,2,1); 
+pl = plot(data.sfe.fp,mean(general.L),'r', 'LineWidth', 1.5, 'DisplayName', 'Mean');
+legend(pl, 'Location','northwest')
+xlabel('Frequency [Hz]')
+ylabel('dP')
+subplot(2,2,3); 
+xlabel('Frequency [Hz]')
+ylabel('dP')
+pr = plot(data.sfe.fp,mean(general.R),'r', 'LineWidth', 1.5, 'DisplayName','Mean'); 
+legend(pr, 'Location','northwest')
+subplot(2,2,2); boxplot(general.L,round(data.sfe.fp,-1))
+%xlabel('Frequency [Hz]')
+subplot(2,2,4); boxplot(general.R,round(data.sfe.fp,-1))
+%xlabel('Frequency [Hz]')
+
+for p=1:2
+    ear = ears(2*p -1);
+    figure(p+1)
+    hold on
+    grid on
+    colorb = colorbar();
+    for i=1:n
+        scatter(1:l.(ear),ones(1,l.(ear)).*data.sfe.fp(i),[],general.(ear)(:,i), 'filled', 'MarkerEdgeColor', 'k')
+    end
+    title(['Quick SFOAE, "' ear '"' ' ear'])
+    xlabel('Number of trial')
+    ylabel('Frequency [Hz]');
+    colorb.Label.String = 'dP';
+end
