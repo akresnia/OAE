704f80d04edcd9f025edcf14c51930361af8b8f0
Alicja Krzesniak <ala.krzesniak@gmail.com>
2018-02-15 14:12:14 +0100

    Revert "Revert "tex file to change language in the licmgr class""
    This reverts commit 2bd1c1170d6d5b55fc86907c15ffee7f960c78bc.

--- analiza_dlugie.m
+++ analiza_dlugie.m
@@ -1,8 +1,12 @@
 %directory_name = 'C:\Users\Alicja\Desktop\praca mgr\moje OAE\20_03\';
-name = 'Kasia';
+names = {'Kasia_K','Magda P','Ewa_K','Agnieszka_K','Krystyna',...
+    'Surala','Klaudia_W', 'Mikolaj_M','Michal_P','Krzysztof_B',...
+    'Justyna_G','Alicja_B'};
+name_idx = 1; 
+name = char(names(name_idx));
 directory_name = ['C:\Users\Alicja\Desktop\praca mgr\OAE ' name '\'];
 SaveFlag = 0;
-y_lim = [-10 20];
+y_lim = [-15 20];
 [a, b, c, short, long, longest] = wczytanie(directory_name);
 %b - 1 - # of long trials
 n = 25; %length(data.sfe.fp)
