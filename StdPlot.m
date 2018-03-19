2c5b6c6109c1b25bca9603665fa78c60ad7b9b11
Alicja Krzesniak <ala.krzesniak@gmail.com>
2018-03-19 14:48:36 +0100

    added 'omitnan' option for noisy data

--- StdPlot.m
+++ StdPlot.m
@@ -5,14 +5,14 @@ st_dev.L = zeros(1,m);
 st_dev.R = zeros(1,m);
 for i = 1:m
     for d = ['L', 'R']
-    st_dev.(d)(i)  = std(data.(d)(:,i));
+    st_dev.(d)(i)  = std(data.(d)(:,i), 'omitnan');
     end
 end
 
 figure()
 hold on
-plot(freqs, st_dev.L, 'DisplayName', 'Left ear')
-plot(freqs, st_dev.R, 'DisplayName', 'Right ear')
+stem(freqs, st_dev.L, 'DisplayName', 'Left ear')
+stem(freqs, st_dev.R, 'DisplayName', 'Right ear')
 %plot(freqs, ones(1,m), 'k--', 'DisplayName', '1 level')
 xlabel('Frequency [Hz]'); ylabel('std (f_i) [dB SPL]')
 xlim([800 6200]); ylim([0 8])
